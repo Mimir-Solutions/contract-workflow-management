bef0f158cdc87e75aa9faec06c325f0ec4718fa5
cyotee doge <cyotee@gmail.com>
2020-12-17 08:14:07 -0800

    Initial project scaffold

---
+++ test/contracts/TestToken1.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.0;
+
+import "hardhat/console.sol";
+
+import "../../contracts/dependencies/holyzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../../contracts/dependencies/holyzeppelin/contracts/math/SafeMath.sol";
+import "../../contracts/dependencies/holyzeppelin/contracts/datatypes/primitives/Address.sol";
+
+contract TestToken1 is ERC20 {
+
+    using SafeMath for uint256;
+    using Address for address;
+
+    constructor () ERC20( "TestToken1", "TT1", 18 ) {
+        console.log("TestToken1::constructor: Instantiating TestToken1");
+        _mint(Context._msgSender(), 50000 * 1**decimals()  );
+        console.log("TestToken1::constructor: Instantiated TestToken1");
+    }
+}
